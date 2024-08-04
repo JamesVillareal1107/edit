b83ce4e473d379330c3df54f9993e52ecb737534
James Villareal <james.villareal123@gmail.com>
2024-08-04 03:47:47 -0700

    Got rid of unnecesary comment

--- src/helper_functions.h
+++ src/helper_functions.h
@@ -1,5 +1,5 @@
 #include <stdbool.h> 
 
 bool process_arguments(int argc, char** argv); 
-bool get_full_directory_path(char* local_directory_name, char full_path[]); // set full_path to the global path equivalent of the input path, return either true or false depending on the success of the operation
+bool get_full_directory_path(char* local_directory_name, char full_path[]); 
 void open_editor_in_directory(char directory[]);  
