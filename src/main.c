50389183294851770e6fa0d405cbea0fc40420f8
James Villareal <james.villareal123@gmail.com>
2024-08-05 18:13:34 -0700

    changed main so that the full path string can hold the largest possible path

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include "helper_functions.h" 
 #include <stdlib.h>
-#include <stdio.h>
+#include <stdio.h> 
+
+#define MAXIMUM_PATH_LENGTH 4097
 
 int main(int argc, char** argv){  
     // parse command line arguments;
@@ -10,11 +12,11 @@ int main(int argc, char** argv){
     }  
 
     // try to find the global path for the second argument, return false if it could not be found, print said path afterwards
-    char full_path[2024]; 
+    char full_path[MAXIMUM_PATH_LENGTH];  
     if(!get_full_directory_path(argv[1], full_path)){  
         printf("FAILED TO FIND PATH\n");
         return 1;
-    } 
+    }
     printf("%s",full_path);  
 
     // open a VScode session in the established global path and terminate the program
